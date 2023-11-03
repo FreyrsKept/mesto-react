33aa66fd7300f59763340f011d38b71dfdf9bd3b
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-11-03 23:17:01 +0700

    global fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="footer">
-            <p class="footer__description">© 2023 Mesto Russia</p>
+            <p className="footer__description">© 2023 Mesto Russia</p>
         </footer>
     )
 }
